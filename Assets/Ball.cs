0e21a4f0713db7e6bc720fd24b9a1c29579281f3
Julian McNeill <julesdowork@gmail.com>
2018-07-17 22:52:32 -0400

    Bounce ball off paddle

---
+++ Assets/Ball.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // radius of ball sprite + half the width of the paddle sprite
+    [SerializeField] float xOffset = 0.26f;
+    [SerializeField] float speed = 4f;
+    [SerializeField] Paddle[] paddles;
+
+    bool isServed = false;
+    Paddle server;
+    Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        AttachBallToServer();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (!isServed)
+        {
+            MoveBallWithServer();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isServed)
+        {
+            Serve();
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    void AttachBallToServer()
+    {
+        paddles = FindObjectsOfType<Paddle>();
+        if (server == paddles[0])
+        {
+            server = paddles[1];
+        }
+        else
+        {
+            server = paddles[0];
+        }
+        float serverPosX = server.transform.position.x;
+        float serverPosY = server.transform.position.y;
+        transform.position = new Vector3(serverPosX + xOffset, serverPosY);
+    }
+
+    void MoveBallWithServer()
+    {
+        float adjustedPosY = server.transform.position.y;
+        transform.position = new Vector3(transform.position.x, adjustedPosY);
+    }
+
+    void Serve()
+    {
+        isServed = true;
+        float randomForceY = Random.Range(-1f, 1f);
+        rigidBody.AddRelativeForce(new Vector2(1f, randomForceY));
+    }
+}
--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paddle : MonoBehaviour {
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float speed = 4f;
+    [SerializeField] float yClamp = 4.3f;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+    {
+		if (Input.GetKey(KeyCode.W))
+        {
+            moveUp();
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moveDown();
+        }
 	}
+
+    void moveUp()
+    {
+        float rawPosY = transform.position.y + speed * Time.deltaTime;
+        float clampPosY = Mathf.Clamp(rawPosY, -yClamp, yClamp);
+
+        transform.position = new Vector3(transform.position.x, clampPosY);
+    }
+
+    void moveDown()
+    {
+        float rawPosY = transform.position.y - speed * Time.deltaTime;
+        float clampPosY = Mathf.Clamp(rawPosY, -yClamp, yClamp);
+
+        transform.position = new Vector3(transform.position.x, clampPosY);
+    }
 }
